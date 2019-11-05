6702d017feda81746bdad5bb9b9e278a6c8b8900
juan.goez <juan.goez@enlared.net.co>
2019-11-05 12:37:02 -0500

    recto2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.number}</span>
+        <button id="inc" onClick={this.increment}>Incrementa</button>
       </div>
     );
+  }
+
+  increment = () => {
+    this.setState({ number: this.state.number + 1});
   }
 }
 
